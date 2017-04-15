d51fda9823e9c5e0c811b2dd4c5c1205fdb110ae
Piotr Kolawa <p.kolawa@gmail.com>
2017-04-15 23:54:18 +0200

    Dodanie nowego descryptora

--- tests/test.js
+++ tests/test.js
@@ -3,14 +3,15 @@ var cheerio = require('cheerio');
 
 $ = cheerio.load(fs.readFileSync('./index.html'));
 
-describe("HTML title", function() {
+describe("HTML elements", function() {
 
   it("Title entity", function() {
     expect($('head title').html().length).toBeGreaterThan(0);
   });
 
-  it("Checking images", function(){
-	//   expect($('img').attribs.src).toBeDefined();
-  });
+});
 
+describe("All elements", function(){
+
+	
 });
