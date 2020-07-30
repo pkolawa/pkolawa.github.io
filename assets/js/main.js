50cd2e41458a0fdec7250b8574b34d4ca8417976
Peter Kolawa <p.kolawa@gmail.com>
2020-07-30 13:11:58 +0200

    Adding wave & shake animation to the project

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('button.scroller').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        document.querySelector(this.getAttribute('data-pointer')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-})
